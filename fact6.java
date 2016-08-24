00bedf15489bf56805bb2114408f0b527f80f9fa
Gowsika1 <gowsikakumaravel@gmail.com>
2016-08-24 09:36:11 +0530

    Create fact6.java

---
+++ fact6.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+class fact6
+{
+  public static void main(String args[])
+  {
+  Scanner sc=new Scanner(System.in);
+  int input1=sc.nextInt();
+  int fact=1;
+  for(int i=1;i<=input1;i++)
+    {
+     int temp=fact*i;
+     fact=temp;
+     }
+  System.out.println(fact);
+  }
+}
